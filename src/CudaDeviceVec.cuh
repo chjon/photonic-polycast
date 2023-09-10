615526f525faaa94bf8498ec6fee2d7ca13750f0
Jonathan Chung <jt2chung@uwaterloo.ca>
2023-09-10 12:16:25 -0400

    move CudaDeviceKernel impl to header

--- src/CudaDeviceVec.cuh
+++ src/CudaDeviceVec.cuh
@@ -29,6 +29,44 @@ namespace PPCast {
     };
 
     template <typename T>
+    CudaDeviceVec<T>::CudaDeviceVec(unsigned int size)
+        : m_data(nullptr)
+        , m_size(size)
+    {
+        if (cudaMalloc((void**)&m_data, sizeof(T) * m_size) != cudaSuccess) m_size = 0;
+    }
+
+    template <typename T>
+    CudaDeviceVec<T>::CudaDeviceVec(const std::vector<T>& data)
+        : m_data(nullptr)
+        , m_size(0)
+    {
+        if (cudaMalloc((void**)&m_data, sizeof(T) * m_size) != cudaSuccess) return;
+
+        if (cudaMemcpy(m_data, data.data(), sizeof(T) * m_size, cudaMemcpyHostToDevice) == cudaSuccess) {
+            m_size = data.size();
+        } else {
+            cudaFree(m_data);
+            m_data = nullptr;
+        }
+    }
+
+    template <typename T>
+    CudaDeviceVec<T>::~CudaDeviceVec() {
+        if (m_size) cudaFree(m_data);
+    }
+
+    template <typename T>
+    bool CudaDeviceVec<T>::copyToDevice(const T* hostData, unsigned int size) {
+        return cudaMemcpy(m_data, hostData, sizeof(T) * size, cudaMemcpyHostToDevice) == cudaSuccess;
+    }
+
+    template <typename T>
+    bool CudaDeviceVec<T>::copyToHost(T* hostData, unsigned int size) const {
+        return cudaMemcpy(hostData, m_data, sizeof(T) * size, cudaMemcpyDeviceToHost) == cudaSuccess;
+    }
+
+    template <typename T>
     inline bool CudaDeviceVec<T>::copyToDevice(const T* hostData) { return copyToDevice(hostData, m_size); }
 
     template <typename T>
